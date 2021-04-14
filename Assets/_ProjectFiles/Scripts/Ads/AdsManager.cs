5816d2ecea8dde82cd03121a8f59a4c057e20702
EveryGameSPlay <repsbro@gmail.com>
2021-04-14 15:39:00 +0300

    Added Google Admob. Ads work only in build.

---
+++ Assets/_ProjectFiles/Scripts/Ads/AdsManager.cs
@@ -0,0 +1,75 @@
+﻿using GoogleMobileAds.Api;
+
+namespace Game.Ads
+{
+    public enum BuildType
+    {
+        Test,
+        Publish
+    }
+    
+    public static partial class AdsManager
+    {
+        public const string UnexpectedPlatformUnitId = "unexpected_platform";
+
+        public static BuildType BuildType { get; set; } = BuildType.Test;
+
+        public enum AdType
+        {
+            Banner,
+            Interstitial,
+            Native,
+            Rewarded
+        }
+
+        public enum MobileOs
+        {
+            Android,
+            IOS,
+            Unexpected
+        }
+        
+        public static string GetUnitId(AdType ad)
+        {
+            switch (GetOs())
+            {
+                case MobileOs.Android:
+                    return GetAndroidUnitId(ad);
+            }
+            
+            return UnexpectedPlatformUnitId;
+        }
+
+        private static string GetAndroidUnitId(AdType ad)
+        {
+            if (BuildType == BuildType.Publish)
+                return UnexpectedPlatformUnitId;
+            
+            switch (ad)
+            {
+                case AdType.Banner:
+                    return "ca-app-pub-3940256099942544/6300978111";
+                case AdType.Interstitial:
+                    return "ca-app-pub-3940256099942544/1033173712";
+                case AdType.Native:
+                    return "ca-app-pub-3940256099942544/2247696110";
+                case AdType.Rewarded:
+                    return "ca-app-pub-3940256099942544/5224354917";
+            }
+
+            return UnexpectedPlatformUnitId;
+        }
+
+        public static MobileOs GetOs()
+        {
+#if UNITY_ANDROID
+            return MobileOs.Android;
+#elif UNITY_IOS
+            return MobileOs.IOS;
+#else
+            return MobileOs.Unexpected;
+#endif
+        } 
+
+    }
+}
---
+++ Assets/_ProjectFiles/Scripts/Ads/AdsManagerRuntime.cs
@@ -0,0 +1,46 @@
+﻿using GoogleMobileAds.Api;
+using UnityEngine;
+
+namespace Game.Ads
+{
+    public static partial class AdsManager
+    {
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        private static void AdsManagerConstructor()
+        {
+            MobileAds.Initialize(x => { });
+        }
+
+        public static BannerView RequestBanner()
+        {
+            var unitId = GetUnitId(AdType.Banner);
+            var bannerView = new BannerView(unitId, AdSize.Banner, AdPosition.Top);
+            bannerView.LoadAd(new AdRequest.Builder().Build());
+
+            return bannerView;
+        }
+
+        public static InterstitialAd RequestInterstitial()
+        {
+            var unitId = GetUnitId(AdType.Interstitial);
+            var interstitial = new InterstitialAd(unitId);
+            interstitial.LoadAd(new AdRequest.Builder().Build());
+            
+            return interstitial;
+        }
+
+        public static RewardedAd RequestRewarded()
+        {
+            var unitId = GetUnitId(AdType.Rewarded);
+            var rewardedAd = new RewardedAd(unitId);
+            rewardedAd.LoadAd(new AdRequest.Builder().Build());
+            
+            return rewardedAd;
+        }
+
+        private static void InterOnAdFailedToLoad(object sender, AdFailedToLoadEventArgs e)
+        {
+            MonoBehaviour.print(e.Message);
+        }
+    }
+}
